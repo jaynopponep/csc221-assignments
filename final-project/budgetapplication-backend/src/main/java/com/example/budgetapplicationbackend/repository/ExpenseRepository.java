e8c2d5304b95754aebdb98e89f4da5441a3346e7
jaynopponep <34385282+jaynopponep@users.noreply.github.com>
2023-12-12 22:07:06 -0500

    Delete final-project/budgetapplication-backend directory

--- final-project/budgetapplication-backend/src/main/java/com/example/budgetapplicationbackend/repository/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.budgetapplicationbackend.repository;
-
-import com.example.budgetapplicationbackend.model.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
-}
